66a160571b7513df3244966ba5990bc43903ba37
krish singh <ks092713@gmail.com>
2026-05-23 19:46:05 +0530

    feat: add routes for interview report generation, retrieval, job searching, and resume analysis

--- services/ai/routes/interview.route.js
+++ services/ai/routes/interview.route.js
@@ -4,32 +4,33 @@ import express from 'express';
 import {interviewController,roastResume, getInterviewReportById, getAllInterviewReportsByUserId, getJobsByTitle} from "../controller/interviewController.js";
 import fileMiddleware from "../middlewares/file.middleware.js";
 import dotenv from "dotenv";
+import userIsAuthenticated from '../shared/isAuthenticated.js';
 dotenv.config({path:"./ai.env"});
 
-const userIsAuthenticated = (req, res, next) => {
-  // Check Authorization header first, then cookie
-  const authHeader = req.headers.authorization;
-  const cookieToken = authHeader?.startsWith("Bearer ") 
-    ? authHeader.split(" ")[1] 
-    : req.cookies.token;
-console.log("Auth header:", authHeader);        // ← add this
-  console.log("Cookie token:", cookieToken);
-  const token=cookieToken;
-  if (!token) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    if (!req.user.id || !req.user.email) {
-      return res.status(401).json({ message: "Unauthorized" });
-    }
-    req.userId = decoded.id;
-    next();
-  } catch (err) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-};
+// const userIsAuthenticated = (req, res, next) => {
+//   // Check Authorization header first, then cookie
+//   const authHeader = req.headers.authorization;
+//   const cookieToken = authHeader?.startsWith("Bearer ") 
+//     ? authHeader.split(" ")[1] 
+//     : req.cookies.token;
+// console.log("Auth header:", authHeader);        // ← add this
+//   console.log("Cookie token:", cookieToken);
+//   const token=cookieToken;
+//   if (!token) {
+//     return res.status(401).json({ message: "Unauthorized" });
+//   }
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = decoded;
+//     if (!req.user.id || !req.user.email) {
+//       return res.status(401).json({ message: "Unauthorized" });
+//     }
+//     req.userId = decoded.id;
+//     next();
+//   } catch (err) {
+//     return res.status(401).json({ message: "Unauthorized" });
+//   }
+// };
 const interviewRouter = express.Router();
 /**
  * @description: takes job description, resume text, self description from frontend and returns the interview report
